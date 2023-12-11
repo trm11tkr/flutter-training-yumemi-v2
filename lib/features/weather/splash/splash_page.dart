c3f25df56c0debfbaa60fe34f8185d1aa59e4574
trm11tkr <mrmrt1121@gmail.com>
2023-12-11 19:10:16 +0900

    Feat: transition to WeatherPage from SplashPage

--- lib/features/weather/splash/splash_page.dart
+++ lib/features/weather/splash/splash_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_training/router.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,9 +12,24 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  static const _waitingTime = Duration(milliseconds: 500);
+
+  Future<void> _goToWeatherPage() async {
+    await SchedulerBinding.instance.endOfFrame;
+    await Future<void>.delayed(_waitingTime);
+    if (!context.mounted) {
+      return;
+    }
+
+    await const WeatherRoute().push<void>(context);
+
+    unawaited(_goToWeatherPage());
+  }
+
   @override
   void initState() {
     super.initState();
+    unawaited(_goToWeatherPage());
   }
 
   @override
