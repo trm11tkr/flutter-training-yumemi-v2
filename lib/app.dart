d0bb38e051be2e09d14b77414b7fd0868798e5cf
trm11tkr <mrmrt1121@gmail.com>
2023-12-11 15:21:52 +0900

    Fix: extract weather page

--- lib/app.dart
+++ lib/app.dart
@@ -1,74 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/temperature_text.dart';
+import 'package:flutter_training/features/weather/weather_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            child: Column(
-              children: [
-                const Spacer(),
-                const Column(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 1,
-                      child: Placeholder(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TemperatureText(
-                              temperature: null,
-                              type: TemperatureType.min,
-                            ),
-                          ),
-                          Expanded(
-                            child: TemperatureText(
-                              temperature: null,
-                              type: TemperatureType.max,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Flexible(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const SizedBox(height: 80),
-                          Expanded(
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Close'),
-                            ),
-                          ),
-                          Expanded(
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Reload'),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: WeatherPage(),
     );
   }
 }
---
+++ lib/features/weather/weather_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_training/features/weather/temperature_text.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            children: [
+              const Spacer(),
+              const Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Placeholder(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TemperatureText(
+                            temperature: null,
+                            type: TemperatureType.min,
+                          ),
+                        ),
+                        Expanded(
+                          child: TemperatureText(
+                            temperature: null,
+                            type: TemperatureType.max,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Flexible(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(height: 80),
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Close'),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Reload'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
