6eab5136f676970d575e8a8d2d4e7d4d8e490b63
trm11tkr <mrmrt1121@gmail.com>
2023-12-13 18:12:22 +0900

    Add: @protected in AfterLayoutMixin property

--- lib/utils/mixin/after_layout_mixin.dart
+++ lib/utils/mixin/after_layout_mixin.dart
@@ -4,6 +4,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
+  @protected
   void afterLayout();
 
   @override
@@ -12,6 +13,7 @@ mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
     unawaited(afterActionFrameCompletes());
   }
 
+  @protected
   Future<void> afterActionFrameCompletes() async {
     await SchedulerBinding.instance.endOfFrame;
     afterLayout();
