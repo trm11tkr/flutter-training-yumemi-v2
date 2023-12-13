1cb99c07fd0b94482d6cbf8783ebbb86d7e56784
trm11tkr <mrmrt1121@gmail.com>
2023-12-13 18:20:44 +0900

    Chore: sorting

--- lib/utils/mixin/after_layout_mixin.dart
+++ lib/utils/mixin/after_layout_mixin.dart
@@ -4,14 +4,14 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
-  @protected
-  void afterLayout();
-
   @override
   void initState() {
     super.initState();
     unawaited(afterActionFrameCompletes());
   }
+
+  @protected
+  void afterLayout();
 
   @protected
   Future<void> afterActionFrameCompletes() async {
